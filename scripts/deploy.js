836e8b92cb3bf6bd0518852db2d4e406acafd227
Travis L Orback <tlorback@gmail.com>
2022-04-09 22:49:03 -0500

    UPDATE: deploy script and deploy app

---
+++ scripts/deploy.js
@@ -0,0 +1,27 @@
+const hre = require("hardhat")
+const fs = require("fs")
+
+async function main() {
+  // We get the contract to deploy
+  const Blog = await hre.ethers.getContractFactory("Blog")
+  const blog = await Blog.deploy("TLO Web3 Blog!")
+
+  await blog.deployed()
+
+  console.log("Blog deployed to:", blog.address)
+
+  fs.writeFileSync(
+    "./config.js",
+    `
+  export const contractAddress = "${blog.address}"
+  export const ownerAddress = "${blog.signer.address}"
+  `
+  )
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
